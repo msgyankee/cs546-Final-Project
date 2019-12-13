f283e3b32496818ef4e656d01b0aab687744be04
amandag0mes <32399201+amandag0mes@users.noreply.github.com>
2019-12-12 19:49:48 -0500

    added favorites to user route

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userData = require("../data/users");
 
-router.get("/:id", async(req, res) => {
-    try{
-        const result = await userData.get(req.params.id);
-        res.json(result);
-    } catch(e){
-        res.status(404).json({error:e});
-    }
-});
 
-router.post("/:id", async(req, res) => {
+router.get("/:id", async(req, res) => {
     try{
         await userData.get(req.params.id);
         const data = userData.userBySession(req.session.loginStatus);
@@ -25,5 +17,26 @@ router.post("/:id", async(req, res) => {
         res.status(500).json({error:e});
     }
 });
+
+router.post("/favorite/:id", async(req, res) => {
+    try{
+        await userData.get(req.params.id);
+        const postID = await postData.create(req.session.loginStatus, req.body.type, req.body.postTitle, req.body.movieTitle, req.body.genre, req.body.content);
+        const user = userData.userBySession(req.session.loginStatus);
+        userID = user._id;
+        if (user == null) res.redirect("/");
+        else {
+            if (user.userdata.isFavorite(userID, postID)) {
+                user.userdata.remFavorite(userID, postID);
+            }
+            else {
+                user.userdata.addFavorite(userID, postID);
+            }
+            res.redirect("/post/");
+        }
+        
+    }
+})
+
 
 module.exports = router;    
